bc52297167ed617aa6a65dc093cb655486a48f06
sunshijiang <ssj1595329871@gmail.com>
2025-06-27 17:45:15 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LD2451-BSM
 Blind Spot Monitoring System
+
+使用xiao-esp32c3进行测试，项目暂未测试完成
--- ld2451-bsm/main/main.c
+++ ld2451-bsm/main/main.c
@@ -16,8 +16,86 @@
 
 static const int RX_BUF_SIZE = 1024;
 
-#define TXD_PIN (GPIO_NUM_4)
-#define RXD_PIN (GPIO_NUM_5)
+#define TXD_PIN (GPIO_NUM_21)
+#define RXD_PIN (GPIO_NUM_20)
+#define LED_PIN (GPIO_NUM_5)
+#define KEY_PIN (GPIO_NUM_9)
+#define GPIO_OUTPUT_PIN_SEL  (1ULL<<LED_PIN)
+#define GPIO_INPUT_PIN_SEL (1Ull<<KEY_PIN)
+
+//中断
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    static int cnt = 0;
+    cnt++;
+    //当中断被触发时，LED状态变化
+    gpio_set_level(LED_PIN, cnt % 2);
+}
+
+void LedGpioConfig(void)
+{
+
+    gpio_config_t gpio_init_struct = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pin_bit_mask = GPIO_OUTPUT_PIN_SEL
+    };
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+
+    gpio_config( &gpio_init_struct );
+    gpio_set_level(LED_PIN, 1);
+}
+
+void KeyGpioConfig(void)
+{
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE,
+        //设置输入引脚
+        .pin_bit_mask = GPIO_INPUT_PIN_SEL,
+        //设置输入模式
+        .mode = GPIO_MODE_INPUT,
+        //使能上拉电阻
+        .pull_up_en = 1,
+        //失能下拉电阻
+        .pull_down_en = 0
+    };
+    gpio_config(&io_conf);
+    gpio_install_isr_service(ESP_INTR_FLAG_EDGE);
+    gpio_isr_handler_add(KEY_PIN, gpio_isr_handler, (void*)NULL);
+    gpio_intr_enable(KEY_PIN);
+
+
+}
+
+bool GetKeyValue(void)
+{
+    //如果按键状态为0
+    if( gpio_get_level(KEY_PIN) == 0 )
+    {
+        //延时消抖，使用该延时需要加入对应头文件
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        // 如果按键状态还是0，说明按键真的按下
+        if( gpio_get_level(KEY_PIN) == 0 )
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void LedOn(void)
+{
+    gpio_set_level(LED_PIN, 0);
+}
+
+void LedOff(void)
+{
+    gpio_set_level(LED_PIN, 1);
+}
+
 
 void init(void)
 {
@@ -72,6 +150,27 @@ static void rx_task(void *arg)
 void app_main(void)
 {
     init();
+    int cnt = 0;
+    int time = 0;
     xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
     // xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 2, NULL);
+
+    LedGpioConfig();
+    KeyGpioConfig();
+    // while(1) {
+    //     //因ESP32S3采用的是RTOS方式运行，必须在死循环中加入延时让其能够正常运转
+    //     vTaskDelay(20 / portTICK_PERIOD_MS);
+
+    //     //如果按键有按下
+    //     if( GetKeyValue() == 0 )
+    //     {
+    //         //使LED状态取反。LED引脚为GPIO48
+    //         gpio_set_level(LED_PIN, cnt = !cnt);
+    //     }
+    // }
+    while(1)
+    {
+        printf("time: %d\n", time++);
+        vTaskDelay(1000 /portTICK_PERIOD_MS);
+    }
 }
